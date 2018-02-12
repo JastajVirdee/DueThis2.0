f480b7f89256c27942a1a05c9bee2cedb9ff7418
JastajVirdee <jastaj.virdee@mail.mcgill.ca>
2018-02-12 17:38:39 -0500

    Added View calandar button

--- app/src/main/java/duethis/MainActivity.java
+++ app/src/main/java/duethis/MainActivity.java
@@ -1,5 +1,6 @@
 package duethis;
 
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -40,5 +41,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 
 }
