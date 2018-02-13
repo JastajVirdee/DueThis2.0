915716b94ceed15952de661278c4d640d967f749
Rahul Amlekar <rahul.amlekar@mail.mcgill.ca>
2018-02-12 19:49:15 -0500

    Imported controller and mocked a controller call - in progress

--- app/src/main/java/duethis/AssignmentActivity.java
+++ app/src/main/java/duethis/AssignmentActivity.java
@@ -1,16 +1,32 @@
 package duethis;
 
+import android.app.DatePickerDialog;
 import android.app.DialogFragment;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.time.Duration;
+import java.util.Calendar;
+
+import controller.DueThisController;
+import controller.InvalidInputException;
+import model.Student;
+
 //import duethis.duethis.R;
 
 
-public class AssignmentActivity extends AppCompatActivity {
+public class AssignmentActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
+    Calendar c = Calendar.getInstance();
+    DueThisController controller = new DueThisController();
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +44,27 @@ public class AssignmentActivity extends AppCompatActivity {
 
         // Click Submit
         Button submitButton = findViewById(R.id.assignmentSubmitButton);
+        final EditText nameField = (EditText) findViewById(R.id.assignmentNameTextfield);
+        String name = nameField.getText().toString();
+
+        final EditText weightField = (EditText) findViewById(R.id.assignmentWeightTextfield);
+        float weight = Float.parseFloat(weightField.getText().toString());
+
+        final EditText estimatedTimeOfCompletionField = (EditText) findViewById(R.id.assignmentEstimatedTimeTextfield);
+        long estimatedTimeOfCompletion = Long.parseLong(estimatedTimeOfCompletionField.getText().toString());
+
+        Duration duration = Duration.ofHours(estimatedTimeOfCompletion);
+        java.sql.Date date = java.sql.Date.valueOf(c.getTime().toString());
+
+        Student student = new Student("testStudentID", "testStudentName");
+
+        try {
+            controller.createAssignment(name, "fakeCourseName", date, weight, duration, student);
+        } catch (InvalidInputException e) {
+            e.printStackTrace();
+        }
+
+
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,6 +84,10 @@ public class AssignmentActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onDateSet(android.widget.DatePicker datePicker, int year, int month, int day){
+        c.set(year, month-1, day, 0, 0);
+    }
 }
 
 
--- app/src/main/java/duethis/DatePicker.java
+++ app/src/main/java/duethis/DatePicker.java
@@ -15,8 +15,7 @@ import java.util.Calendar;
  */
 
 
-public class DatePicker extends DialogFragment
-        implements DatePickerDialog.OnDateSetListener {
+public class DatePicker extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -27,17 +26,11 @@ public class DatePicker extends DialogFragment
         int day = c.get(Calendar.DAY_OF_MONTH);
 
         // Create a new instance of DatePickerDialog and return it
-        return new DatePickerDialog(getActivity(), this, year, month, day);
+        return new DatePickerDialog(getActivity(), (DatePickerDialog.OnDateSetListener) getActivity(), year, month, day);
     }
 
     public void onDateSet(DatePicker view, int year, int month, int day) {
         // Do something with the date chosen by the user
     }
-
-    @Override
-    public void onDateSet(android.widget.DatePicker datePicker, int i, int i1, int i2) {
-
-    }
-
 
 }
