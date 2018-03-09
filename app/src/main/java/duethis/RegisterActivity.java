3ab8ea0445d4a7a0912d248a67f9247e55b4cbce
OlivierSM <olivier.morissette@gmail.com>
2018-03-09 18:27:50 -0500

    - Updated the jar file to use the new jar file from Aurelie's updates. - Edited the frontend to have both controllers. - Added a login and register activity with all the necessary fields

---
+++ app/src/main/java/duethis/RegisterActivity.java
@@ -0,0 +1,28 @@
+package duethis;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        getSupportActionBar().setTitle("Register");
+        // used to get global student variable.
+        final DueThisApplication application = (DueThisApplication) this.getApplication();
+
+        // Click Login Button
+        Button loginButton = findViewById(R.id.registerLoginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
