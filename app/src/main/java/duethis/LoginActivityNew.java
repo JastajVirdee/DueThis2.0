6b26af53baa6447c71704cc78f7ab3d224eb3441
Matthew Phillips <matthewp1234@gmail.com>
2018-03-09 19:28:12 -0500

    almost doneeeeeeeeeeeeeeeee

--- app/src/main/java/duethis/LoginActivityNew.java
+++ app/src/main/java/duethis/LoginActivityNew.java
@@ -10,8 +10,8 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import controller.DueThisController;
-
-//import controller.AccountController; //////////////////////////////////////////////////////////////////////////////////////
+import controller.AccountController;
+import controller.InvalidInputException;
 
 
 import model.Student;
@@ -25,9 +25,11 @@ public class LoginActivityNew  extends AppCompatActivity {
     private EditText pwordField;
     private String thisPword;
 
+    Student s;
 
+    //getting controller stuff
     DueThisController controller = new DueThisController();
-    // AccountController acctController = new AccountController(); //////////////////////////////////////////////////////
+    AccountController acctController = new AccountController();
 
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -37,6 +39,28 @@ public class LoginActivityNew  extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_new);
         getSupportActionBar().setTitle("Login");
+
+
+        // Click Back
+        Button backButton = findViewById(R.id.hoursAvailableBackButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivityNew.this, MainActivity.class));
+            }
+        });
+
+
+        // Click Register Button
+        Button registerButton = findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivityNew.this, RegisterActivity.class));
+            }
+        });
+
+
 
         // Click Login Button
         Button loginSubmitButton = findViewById(R.id.loginSubmitButton);
@@ -52,20 +76,21 @@ public class LoginActivityNew  extends AppCompatActivity {
                 pwordField = findViewById(R.id.loginPasswordField);
                 String pwordText = pwordField.getText().toString();
 
-
-                /*
-                Student s = acctController.logIn(unameText, pwordText);
-
+                try {
+                    Student s = acctController.logIn(unameText, pwordText);
+                } catch (InvalidInputException e) {
+                    e.printStackTrace();
+                }
 
                 if(s == null) // null if login is invalid
                 {
-
+                    //TODO print that login was unsuccessful
                 }
                 else
                 {
-                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    startActivity(new Intent(LoginActivityNew.this, MainActivity.class));
                 }
-                */
+
 
 
             }
--- app/src/main/java/duethis/MainActivity.java
+++ app/src/main/java/duethis/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity  implements DatePickerDialog
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, MainActivity.class));
+                startActivity(new Intent(MainActivity.this, LoginActivityNew.class));
             }
         });
 
